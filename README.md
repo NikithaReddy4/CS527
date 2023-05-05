432af1af130390c79292b370dce8fccf3691bf0e
Nikitha Reddy Sankepally <nikithareddysankepally@Nikithas-MacBook-Pro.local>
2023-05-04 22:02:31 -0500

    result analysis

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-Steps to run the project and test suite:
+Mutation Testing Framework for Jsoup Application
 
-1. Run “mvn test” command in terminal on exisiting directory to generate .class files of all the Java files of jsoup project and run test suite. 
-2. Run CodeModifierTest class to generate mutants
-3. The mutated code will be present in the “new_code” directory.
-4. Now, change the pom.xml file by adding parameter “sourceDirectory” pointing to modified code which is “new_code” : Paste the below command and add that element inside “build”.
-<sourceDirectory>new_code<sourceDirectory>
-5.Finally, run “mvn test” in terminal to test the mutated code against the test suite.
+This project is a mutation testing framework for Jsoup application. It uses mutation testing to evaluate the quality of test suites by introducing small changes (mutants) to the application at the source code level and check if the tests can detect these mutants.
+
+The framework consists of a set of mutators, each of which introduces a specific type of mutation to the code. The framework uses Maven to compile and run the tests against each mutant.
+
+Functions of the CodeModifierTest class:
+
+runMavenTests: This function runs the mvn test command, handles the timeout using threads, and generates the Surefire report for each mutant.
+getJavaFilesAndApplyMutators: This function consists of the implementation which includes the following: 
+a. Firstly, it iterates over the mutators loop. 
+b. Then, tracks the java files in the jsoup directory. 
+c. Further, for each file, it creates an object of the chosen mutator class and calls the visit function of the class. The visit function returns the list of mutants for each file from the mutator class. 
+d. Next, it iterates over the list of mutants and calls the runMavenTests function to run the test suite against each mutant, generate the Surefire reports, and write the details like mutants generated and mutants killed details into a result file. 
+e. It then replaces the java file with the original contents of the file.
+
+deleteFolderContents: This function deletes the set of Surefire reports that are generated as a part of one mutation testing execution and is called every time before executing the mutation testing from the main.
+Execution flow of the project:
+
+Trigger the mvn test command for the first time, which will generate the .class files.
+Run the getJavaFilesAndApplyMutators function to execute the flow accordingly as mentioned above.
+Usage: To use this framework, clone the repository and run the mvn test which generates the class files and makes sure that all the testcases are run succesfully on the unmodified code. Then execute the main function in getJavaFilesAndApplyMutators to start the mutation testing.
+
+Results: The required surefire reports will be present under report folder in the repository. The metrics for each operator like mutants generated and killed will be present under results folder in repository.
+
+Dependencies:
+
+Java 8 or higher
+Maven 3 or higher
+Contributors: Nikitha Reddy Sankepally(nikitha6) Eesha Rajesh Chawhan(chawhan2)
--- src/main/java/org/jsoup/CodeModifierTest.java
+++ src/main/java/org/jsoup/CodeModifierTest.java
@@ -35,94 +35,6 @@ public class CodeModifierTest
 		return "";
 	}
 
-
-//	public void getJavaFiles() {
-//		// Initialize the source root as the "target/test-classes" dir, which
-//		// includes the test resource information (i.e., the source code info
-//		// for Jsoup for this assignment) copied from src/test/resources
-//		// during test execution
-//		SourceRoot sourceRoot = new SourceRoot(
-//				CodeGenerationUtils.mavenModuleRoot(CodeModifierTest.class)
-//						.resolve("target/classes"));
-//		String testDirPath = System.getProperty("user.dir")+"/src/main/java/org/jsoup";
-//		String dir_path="";
-//		Path path = Paths.get(testDirPath);
-//		int count=0;
-//		List<String> javaFileNames = new ArrayList<>();
-//		File directory = new File(testDirPath);
-//		File[] files = directory.listFiles();
-//		for (File file : files) {
-//			dir_path="";
-//			if(file.isDirectory())
-//			{
-//				dir_path=testDirPath+"/"+file.getName();
-//				File dir_file = new File(dir_path);
-//				File[] dir_files = dir_file.listFiles();
-//				List<String> filePaths = new ArrayList<>();
-//				if (dir_files != null) {
-//				for (File d_file : dir_files) {
-//
-//					if (d_file.isFile() && d_file.getName().endsWith(".java")) {
-//						String filePath = d_file.getAbsolutePath();
-//						String packageName = getPackageName(filePath);
-//						String fileName = d_file.getName();
-//						CompilationUnit cu = sourceRoot.parse(dir_path, fileName);
-//						System.out.println(packageName + "/" + fileName);
-//						//just select a few files from the java files and call the operators on them TODO:
-//						//call specific operator randomly TODO:
-////						Random rand = new Random();
-////						int randomNumber = rand.nextInt(4) + 1;
-////						System.out.println("N:" + randomNumber);
-////						int mutator=1;
-////						switch (mutator) {
-////							case 1:
-////								if(count<1) {
-////									count++;
-////									CodeModifier codeModifier = new CodeModifier();
-////
-////									codeModifier.visit(cu, null);
-////								}
-////								break;
-////
-////							default:
-////								System.out.println("default");
-////
-////						}
-//						File outputFile = fileInGeneratedTestSourcesDirectory(packageName, fileName);
-//						outputFile.getParentFile().mkdirs();
-//						try (FileWriter outputWriter = new FileWriter(outputFile)) {
-//							outputWriter.write(cu.toString());
-//						} catch (IOException e) {
-//							e.printStackTrace();
-//						}
-//					}
-//
-//
-//	}
-//}
-//else if (file.isFile() && file.getName().endsWith(".java")) {
-//					System.out.println(getPackageName(file.getAbsolutePath())+"  "+file.getName());
-//					CompilationUnit cu = sourceRoot.parse(testDirPath,file.getName());
-//					// Instantiate the CodeModifier class that you will implement to perform
-//					// the actual task. This is a visitor class according to the visitor
-//					// pattern (one of the most important design patterns).
-//					CodeModifier codeModifier = new CodeModifier();
-//					// Apply our visitor class on the code representation for the given
-//					// Java file. In this way, our visit function(s) can be automatically
-//					// applied to all possible elements of the specified type(s).
-//					codeModifier.visit(cu, null);
-//					File outputFile = fileInGeneratedTestSourcesDirectory(getPackageName(file.getAbsolutePath()), file.getName());
-//					// Write the modified code to the new file
-//        			outputFile.getParentFile().mkdirs();
-//					try (FileWriter outputWriter = new FileWriter(outputFile)) {
-//					outputWriter.write(cu.toString());
-//					}catch (IOException e) {
-//					// Handle the exception
-//					e.printStackTrace();
-//}}}
-//}
-//}
-
 	public static int runMavenTests(int i) throws IOException, InterruptedException {
 		String[] mvnCommand = {"mvn", "test"};
 		ProcessBuilder processBuilder = new ProcessBuilder(mvnCommand);
@@ -168,25 +80,11 @@ public class CodeModifierTest
 		reportBuilder.redirectErrorStream(true);
 		Process reportProcess = reportBuilder.start();
 		BufferedReader reportReader = new BufferedReader(new InputStreamReader(reportProcess.getInputStream()));
-//		while ((line = reportReader.readLine()) != null) {
-//
-//			if (line.contains("Failures:")) {
-//				String[] parts = line.split(",");
-//				for (String part : parts) {
-//					if (part.contains("Failures:")) {
-//						System.out.println(Integer.parseInt(part.split(":")[1].trim()));
-//					}
-//				}
-//			}
-//		}
-
 		int reportExitCode = reportProcess.waitFor();
 		Path reportSourcePath = Paths.get(System.getProperty("user.dir"), "target", "site", "surefire-report.html");
 		Path reportDestPath = Paths.get(System.getProperty("user.dir"), "report", "surefire-report"+i+".html");
-
 		Files.createDirectories(reportDestPath.getParent());
 		Files.copy(reportSourcePath, reportDestPath, StandardCopyOption.REPLACE_EXISTING);
-
 		if(exitCode==0)
 		{
 			System.out.println("Mutant Not Killed");
@@ -198,7 +96,7 @@ public class CodeModifierTest
 		return exitCode;
 	}
 
-	public void getJavaFiles() throws IOException {
+	public void getJavaFilesAndApplyMutators() throws IOException {
 		// Initialize the source root as the "target/test-classes" dir, which
 		// includes the test resource information (i.e., the source code info
 		// for Jsoup for this assignment) copied from src/test/resources
@@ -212,18 +110,20 @@ public class CodeModifierTest
 		List<String> javaFileNames = new ArrayList<>();
 		File directory = new File(testDirPath);
 		File[] files = directory.listFiles();
-		List<CompilationUnit> mutants=new ArrayList<>();
-		Map<CompilationUnit, String> mutants_map=Collections.emptyMap();
+		List<CompilationUnit> mutants = new ArrayList<>();
+		Map<CompilationUnit, String> mutants_map = Collections.emptyMap();
 		String dummdir = System.getProperty("user.dir") + "/src/";
-
+		String resultpath = System.getProperty("user.dir") + "/results/";
+		FileWriter writer = new FileWriter(resultpath+"result.txt");
 		int count = 0;
 		int tests = 0;
 		int filecount = 0;
 		int mutantkilled = 0;
 		int m = 0;
 		int mutantgenerated = 0;
-		for (int mutationoperator = 1; mutationoperator <2; mutationoperator++){
-
+		int mutationoperator=0;
+		//iterator for the mutators, in each iterator one mutator is picked and is applied to all the java files
+		for (mutationoperator = 1; mutationoperator < 2; mutationoperator++) {
 			for (File file : files) {
 				dir_path = "";
 				if (file.isDirectory()) {
@@ -232,6 +132,7 @@ public class CodeModifierTest
 					File[] dir_files = dir_file.listFiles();
 					List<String> filePaths = new ArrayList<>();
 					if (dir_files != null) {
+						//iterator for all java files 
 						for (File d_file : dir_files) {
 							if (d_file.isFile() && d_file.getName().endsWith(".java")) {
 								String filePath = d_file.getAbsolutePath();
@@ -239,7 +140,7 @@ public class CodeModifierTest
 								String fileName = d_file.getName();//Attributes.java
 								CompilationUnit cu = sourceRoot.parse(dir_path, fileName);
 								//CompilationUnit scu = sourceRoot.parse(dir_path, fileName);
-								if (!(fileName.equals("CodeModifierTest.java")) && !(fileName.equals("CodeModifier.java"))) {
+								if (filecount<1&&!(fileName.equals("CodeModifierTest.java")) && !(fileName.equals("CodeModifier.java"))) {
 									{
 										filecount++;
 										File og = new File(filePath);
@@ -248,104 +149,102 @@ public class CodeModifierTest
 										og.renameTo(copy);
 										//Map<operator,map<file,list<mutants>>
 										//Map<operator,mutationscore>
-switch(mutationoperator) {
-	//NegateConditions operator = new NegateConditions(cu);
-	//operator.visit(cu, fileName);
+										switch (mutationoperator) {
+											//NegateConditions operator = new NegateConditions(cu);
+											//operator.visit(cu, fileName);
 
-	//ConditionalsBoundary operator = new ConditionalsBoundary(cu);
-	//operator.visit(cu, fileName);
+											//ConditionalsBoundary operator = new ConditionalsBoundary(cu);
+											//operator.visit(cu, fileName);
 
 //									Increments operator = new Increments(cu);
 //									operator.visit(cu, fileName);
 
-	//	AssignIncrements operator = new AssignIncrements(cu);
-	//    operator.visit(cu, fileName);
+											//	AssignIncrements operator = new AssignIncrements(cu);
+											//    operator.visit(cu, fileName);
 
-	//	FalseReturns operator = new FalseReturns(cu);
-	//	    operator.visit(cu, fileName);
+											//	FalseReturns operator = new FalseReturns(cu);
+											//	    operator.visit(cu, fileName);
 
-	//	TrueReturns operator = new TrueReturns(cu);
-	//	operator.visit(cu, fileName);
+											//	TrueReturns operator = new TrueReturns(cu);
+											//	operator.visit(cu, fileName);
 
-	//		EmptyReturns operator = new EmptyReturns(cu);
-	//			operator.visit(cu, fileName);
+											//		EmptyReturns operator = new EmptyReturns(cu);
+											//			operator.visit(cu, fileName);
 
-	//	VoidMethodCall operator = new VoidMethodCall(cu);
-	//			operator.visit(cu, fileName);
+											//	VoidMethodCall operator = new VoidMethodCall(cu);
+											//			operator.visit(cu, fileName);
 
-	case 0:
-		NegateConditions operator = new NegateConditions(cu);
-		operator.visit(cu, fileName);
-		mutantgenerated += operator.getMutants().size();
-		System.out.println("Mutations in " + fileName + "\n");
-		mutants_map = operator.getMutantsMap();
-		mutants = operator.getMutants();
-		break;
-	case 1:
-		ConditionalsBoundary conditionalsBoundary = new ConditionalsBoundary(cu);
-		conditionalsBoundary.visit(cu, fileName);
-		mutantgenerated += conditionalsBoundary.getMutants().size();
-		System.out.println("Mutations in " + fileName + "\n");
-		mutants_map = conditionalsBoundary.getMutantsMap();
-		mutants = conditionalsBoundary.getMutants();
-		break;
-	default:
-		break;
-}
-
-										for (int i = 0; i < mutants.size(); i++) {
-											m++;
-											File outputFile = fileInGeneratedTestSourcesDirectory(filePath);
-											// Write the modified code to the new file which will overwrite the original file
-											outputFile.getParentFile().mkdirs();
-											try (FileWriter outputWriter = new FileWriter(outputFile)) {
-												//System.out.println("The mutants are"+codeModifier.getMutants().get(i).toString());
-												System.out.println(mutants_map.get(mutants.get(i)));
-												outputWriter.write(mutants.get(i).toString());
-											} catch (IOException e) {
-												e.printStackTrace();
-											}
-											try {
-												if (runMavenTests(m) == 0) {
-													System.out.println("passed " + m + "\n");
-												} else {
-													mutantkilled++;
-													System.out.println("failed" + "\n");
-												}
-											} catch (InterruptedException e) {
-												throw new RuntimeException(e);
-											}
-											//restore the file
+											case 0:
+												NegateConditions operator = new NegateConditions(cu);
+												operator.visit(cu, fileName);
+												mutantgenerated += operator.getMutants().size();
+												System.out.println("Mutations in " + fileName + "\n");
+												mutants_map = operator.getMutantsMap();
+												mutants = operator.getMutants();
+												break;
+											case 1:
+												ConditionalsBoundary conditionalsBoundary = new ConditionalsBoundary(cu);
+												conditionalsBoundary.visit(cu, fileName);
+												mutantgenerated += conditionalsBoundary.getMutants().size();
+												System.out.println("Mutations in " + fileName + "\n");
+												mutants_map = conditionalsBoundary.getMutantsMap();
+												mutants = conditionalsBoundary.getMutants();
+												break;
+											default:
+												break;
 										}
-
-
+											for (int i = 0; i < mutants.size(); i++) {
+												m++;
+												File outputFile = fileInGeneratedTestSourcesDirectory(filePath);
+												// Write the modified code to the new file which will overwrite the original file
+												outputFile.getParentFile().mkdirs();
+												try (FileWriter outputWriter = new FileWriter(outputFile)) {
+													//System.out.println("The mutants are"+codeModifier.getMutants().get(i).toString());
+													System.out.println(mutants_map.get(mutants.get(i)));
+													outputWriter.write(mutants.get(i).toString());
+												} catch (IOException e) {
+													e.printStackTrace();
+												}
+												try {
+													if (runMavenTests(m) == 0) {
+														System.out.println("passed " + m + "\n");
+													} else {
+														mutantkilled++;
+														System.out.println("failed" + "\n");
+													}
+												} catch (InterruptedException e) {
+													throw new RuntimeException(e);
+												}
+												//restore the file
+											}
 //									if (codeModifier.getMutants().isEmpty()) {
 //										System.out.println("No mutants generated");
 //									}
-									}
-									try {
-										// Create input and output streams for the files
-										Path sourceFile = Paths.get(dummdir+fileName);
-										File sf = new File(dummdir+fileName);
-										Path destinationFile = Paths.get(filePath);
-
-										Files.copy(sourceFile, destinationFile, StandardCopyOption.REPLACE_EXISTING);
-
-										// Delete the source file
-										if (sf.delete()) {
-											System.out.println("File copied and deleted successfully." + "\n");
-										} else {
-											System.out.println("Failed to delete source file.");
 										}
+										try {
+											// Create input and output streams for the files
+											Path sourceFile = Paths.get(dummdir + fileName);
+											File sf = new File(dummdir + fileName);
+											Path destinationFile = Paths.get(filePath);
 
-									} catch (IOException e) {
-										System.out.println("An error occurred.");
-										e.printStackTrace();
+											Files.copy(sourceFile, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+
+											// Delete the source file
+											if (sf.delete()) {
+												System.out.println("File copied and deleted successfully." + "\n");
+											} else {
+												System.out.println("Failed to delete source file.");
+											}
+
+										} catch (IOException e) {
+											System.out.println("An error occurred.");
+											e.printStackTrace();
+										}
+										System.out.println("mutantgenerated" + mutantgenerated);
+										System.out.println("mutantkilled" + mutantkilled + "\n");
 									}
-									System.out.println("mutantgenerated" + mutantgenerated);
-									System.out.println("mutantkilled" + mutantkilled + "\n");
 								}
-							}}
+							}
 //						else if (file.isFile() && file.getName().endsWith(".java")) {
 //
 //							//System.out.println(getPackageName(file.getAbsolutePath())+"  "+file.getName());
@@ -391,7 +290,10 @@ switch(mutationoperator) {
 					}
 				}
 			}
-	}
+		writer.write(mutationoperator+"\n"+"mutantgenerated"+mutantgenerated+"\n"+"mutantkilled"+mutantkilled);
+        writer.close();
+    }
+
 
 	public void deleteFolderContents(File folder) {
 		File[] files = folder.listFiles(); // get all files and subdirectories within the folder
@@ -409,7 +311,7 @@ switch(mutationoperator) {
 		//generate a random number to set the number of mutants, and have a for loop to generate that many mutants TODO:
 		//lets keep a copy of dir
 		cm.deleteFolderContents(new File(System.getProperty("user.dir")+"/report/"));
-		cm.getJavaFiles();
+		cm.getJavaFilesAndApplyMutators();
 		// copy the surefire from target/site/surefire-report.html to a report folder
 		//runMavenTests(1);
 
